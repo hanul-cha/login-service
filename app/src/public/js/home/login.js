258c175c925b04f077f29d83a8038671ceb536db
hanul-cha <ckgks3014@naver.com>
2021-09-01 13:50:53 +0900

    create fetch for login

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,25 @@
-console.log("hi");
-console.log("rehi");
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#pasword"),
+    loginBtn = document.querySelector("button");
+
+
+loginBtn.addEventListener('click',login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+
+    fetch('./login', {
+        method: "POST",
+        header: {
+            "Content-Type": "application/json",
+        },
+        bldy: JSON.stringify(req),
+    });
+}
+
+
+
+
