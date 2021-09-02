dee9661f673316a60c33d9ba00c54800e73ee0da
hanul-cha <ckgks3014@naver.com>
2021-09-02 16:33:29 +0900

    create register page ui

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,48 @@
+const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
+    psword = document.querySelector("#psword"),
+    confirmPsword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("button");
+
+const submit = document.querySelector("form");
+
+function onSubmit(e) {
+    e.preventDefault();
+}
+
+submit.addEventListener("submit",onSubmit);
+
+registerBtn.addEventListener('click',register);
+
+function register() {
+    const req = {
+        id: id.value,
+        name: name.value,
+        psword: psword.value,
+        confirmPsword: confirmPsword.value,
+    };
+    console.log(req);
+
+    fetch('/register', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    })
+    .then((res) => res.json())//프라미스로 반환됨 그래서 아래에 then을 한번더 불러줌
+    .then((res) => {
+        if(res.success) {
+            location.href = '/login';
+        } else {
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.error('회원가입 중 에러 발생');
+    });
+}
+
+
+
+
