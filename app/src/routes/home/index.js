5a60cf2918db29f7ad40d819e4d783f1d5bcc912
hanul-cha <ckgks3014@naver.com>
2021-09-03 15:35:03 +0900

    required tag value

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const ctrl = require('./home.ctrl')
+const ctrl = require('./home.ctrl');
 
 router.get('/', ctrl.output.home);
 router.get('/login', ctrl.output.login);
