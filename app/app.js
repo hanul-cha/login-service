23b3efa7fd185df4152689f40bf8e9f72167be18
hanul-cha <ckgks3014@naver.com>
2021-09-08 14:42:32 +0900

    morgan을 사용한 로그관리

--- app/app.js
+++ app/app.js
@@ -4,12 +4,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
+const fs = require("fs");
+
 
 const app = express();
+dotenv.config();
 
 // 라우팅
 const home = require("./src/routes/home");
+
+const accessLogStream = fs.createWriteStream(
+    `${__dirname}/logs/access.log`,
+    { flags: "a" }
+);
 
 // 앱 세팅
 app.set("views", "./src/views");
@@ -18,6 +26,7 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev", { stream: accessLogStream }));//로그를 찍기위한 미들웨어
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
