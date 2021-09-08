79cffd1cd37f38f7db8bdc912ce1eae895f8cee1
hanul-cha <ckgks3014@naver.com>
2021-09-08 14:04:58 +0900

    dotenv로 환경변수 등록

--- app/bin/www.js
+++ app/bin/www.js
@@ -3,8 +3,8 @@
 const app = require("../app");
 const logger = require("../src/config/logger");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; //환경변수로 셋팅해 env폴더의 토트값을 사용하고 없으면 3000으로 실행하겠다는 의미
 
-app.listen(PORT, () => {
+app.listen(PORT, () => { 
   logger.info(`${PORT} 포트에서 서버가 가동되었습니다.`);
 });
